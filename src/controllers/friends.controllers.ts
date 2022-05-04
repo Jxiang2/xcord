4e30fea9e2a944bfe27389f20d33f28759d2e114
Jingyu Xiang <jxiang2@ualberta.ca>
2022-05-03 23:30:58 -0600

    add comments

--- src/controllers/friends.controllers.ts
+++ src/controllers/friends.controllers.ts
@@ -1,5 +1,10 @@
 import {Request, Response} from "express";
 
+/**
+ * send friend invitation
+ * @param req
+ * @param res
+ */
 const postInvite = async (req: Request, res: Response) => {
   const {targetMail} = req.body;
   return res.send(`controller is sending: ${targetMail}`);
